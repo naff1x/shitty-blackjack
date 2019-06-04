568de1fdec23ef71b2a4b3ea920bf11bb748034f
naff1x <naffixemail@gmail.com>
2019-06-04 13:23:58 +0200

    Added function for opening window.

--- crashsite.py
+++ crashsite.py
@@ -1,9 +1,16 @@
 def basicImport():
     import os
-    os.system('pip3 install --user arcade')
-    print("* Finished library install *")
-    import arcade
-    print("* Using Arcade version",arcade.VERSION,"*")
+    os.system('pip3 install --user arcade') #Installs files necessary for the 'Arcade' library.
+    print("* Finished library install *") 
+
+def openWindow():
+    width = int(input("Enter window width (px): "))
+    height = int(input("Enter window height (px): "))
+    arcade.open_window(width,height,"Test") #This line causes MaxOS to generate a ApplePersistenceIgnoreState warning. Enter command 'defaults write org.python.python ApplePersistenceIgnoreState NO' to solve.
+    arcade.run()
 
 if __name__ == "__main__": #Runs indented code if this file is regarded as "main"
-    basicImport()
+    #basicImport()
+    import arcade #Imports 'Arcade' library so that we can actually use it in the file.
+    print("* Using Arcade version",arcade.VERSION,"*")
+    openWindow()
