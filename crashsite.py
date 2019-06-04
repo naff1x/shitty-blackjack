00ebf7ecbfd5f9497618e70c9d1768d22a825ca1
naff1x <naffixemail@gmail.com>
2019-06-04 16:52:08 +0200

    Removed function 'openWindow()'.

--- crashsite.py
+++ crashsite.py
@@ -11,14 +11,10 @@ if sys.platform == "win32": #If the user's OS = Windows...
 
 import arcade
 
-def openWindow():
-    width = int(input("Enter window width (px): "))
-    height = int(input("Enter window height (px): "))
-    arcade.open_window(width,height,"Window") #This line causes MaxOS to generate a ApplePersistenceIgnoreState warning. Enter command 'defaults write org.python.python ApplePersistenceIgnoreState NO' to solve.
-    arcade.run()
-    
+#arcade.open_window(width,height,"Window") #This line causes MacOS to generate a ApplePersistenceIgnoreState warning. Enter command 'defaults write org.python.python ApplePersistenceIgnoreState NO' to solve.
+
 class GameWindow(arcade.Window):
-    def __init__(self, width=800, height=600, title='Arcade Window', fullscreen=False, resizable=False, update_rate=1 /60, antialiasing=True):
+    def __init__(self, width=800, height=600, title='Shitty Blackjack', fullscreen=False, resizable=False, update_rate=1 /60, antialiasing=True):
         return super().__init__(width=width, height=height, title=title, fullscreen=fullscreen, resizable=resizable, update_rate=update_rate, antialiasing=antialiasing)
 
 #   WRITE CODE ABOVE THIS LINE
@@ -26,5 +22,5 @@ if __name__ == "__main__": #Runs indented code if this file is regarded as "main
     #import arcade #Imports 'Arcade' library so that we can actually use it in the file.
     print("* Using Arcade version",arcade.VERSION,"*")
     #openWindow()
-    unit = GameWindow(700,400,"Shitty Blackjack",False,True)
+    unit = GameWindow()
     arcade.run()
