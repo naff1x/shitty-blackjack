96ae70180478f60f364a59cb1628de3cb4726413
naff1x <naffixemail@gmail.com>
2019-06-10 16:21:35 +0200

    Added blitting (rendering) and darkened background.

--- crashsite.py
+++ crashsite.py
@@ -24,10 +24,25 @@ def setup():
     # Set background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill((190, 0, 0))
+    background.fill((150, 0, 50))
+
+    # Blit to screen (render)
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+    eventLoop(screen, background)
+
+
+def eventLoop(screen, background):
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
 
 def main():
     setup()
+
 
 main()  # Last line of code, runs main code.
