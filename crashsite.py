d1111e9094ad4e41c618ad43bd4727d89a2b552e
naff1x <naffixemail@gmail.com>
2019-06-11 19:09:40 +0200

    Added background image.

--- crashsite.py
+++ crashsite.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+""" UN-COMMENT THIS CODE BEFORE RUNNING IT ON A COMPUTER FOR THE FIRST TIME!
 import sys
 import os
 
@@ -10,12 +11,12 @@ if sys.platform == "darwin":  # If the user's OS = MacOS...
 if sys.platform == "win32":  # If the user's OS = Windows...
     os.system('pip install --user pygame')
     print("* Finished library install *")
-
+"""
 import pygame
 print("* Using PyGame version", pygame.__version__.__str__(), "*")
 
 x = 20
-y = 20
+y = 200
 width = 20
 height = 20
 vel = 5
@@ -32,7 +33,9 @@ def setup():
     for i in range(1, len(bgmusic)):  # Queues the remaining songs
         pygame.mixer_music.queue(bgmusic[i])
         print("√ Queued song: ", bgmusic[i])
-    pygame.mixer_music.set_volume(0.001)  # Set to 1 at start of 'eventLoop'
+    pygame.mixer_music.set_volume(0.001)
+    """ Volume is set to 1 at start of 'eventLoop'. This is done to avoid...
+    a scratching noise when a song is loaded and played. """
     pygame.mixer_music.play(-1)
 
     # Create screen
@@ -41,9 +44,12 @@ def setup():
     pygame.display.set_caption('Shitty Blackjack')
 
     # Set background
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill((150, 0, 50))
+    try:
+        background = pygame.image.load("images/mainMenu.jpg").convert()
+    except FileNotFoundError():
+        background = pygame.Surface(screen.get_size())
+        background = background.convert()
+        background.fill((150, 0, 50))
 
     # Set cursor (optional)
     pygame.mouse.set_cursor(*pygame.cursors.broken_x)
@@ -75,7 +81,7 @@ def eventLoop(screen, background):
         if keys[pygame.K_BACKSPACE]:
             loopBool = False
 
-        screen.fill((150, 0, 50))
+        screen.blit(background, (0, 0))
         pygame.draw.rect(screen, (0, 0, 255), (x, y, width, height))
         pygame.display.update()
 
