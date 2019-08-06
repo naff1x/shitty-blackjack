ee8fc3152e1f97435f2218309d4b45f7f0da77e1
naff1x <naffixemail@gmail.com>
2019-08-06 08:57:13 +0200

    Second draft. This version is fully functional.

--- cards.py
+++ cards.py
@@ -4,77 +4,41 @@ class Deck:
     def __init__(self):
         newDeck = 52*[None]
 
-        for a in range(4):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(2, i)
-        for a in range(4, 8):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(3, i)
-        for a in range(8, 12):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(4, i)
-        for a in range(12, 16):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(5, i)
-        for a in range(16, 20):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(6, i)
-        for a in range(20, 24):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(7, i)
-        for a in range(24, 28):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(8, i)
-        for a in range(28, 32):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(9, i)
-        for a in range(32, 36):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(10, i)
-        for a in range(36, 40):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(11, i)
-        for a in range(40, 44):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(12, i)
-        for a in range(44, 48):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(13, i)
-        for a in range(48, 52):
-            for i in range(4):
-                print(a, i)
-                newDeck[a] = Card(14, i)
-        
-        #for i in range(52):
-            #print(newDeck[i].rank, newDeck[i].suit)
+
+        position = 0
+
+        for rank in range (13):
+            for suit in range(4):
+                newDeck[position] = Card(rank+2, suit)
+                position += 1
+
+        #for i in range(len(newDeck)):
+            #print(str(newDeck[i].rank) + " " + str(newDeck[i].suit))
 
 class Card:
-    def __init__(self, rank, inputSuit):
+    def __init__(self, inRank, inSuit):
         # "rank" meaning number or face and "suit" meaning clubs, diamonds etc.
-        self.rank = rank
-        if inputSuit == 0:
+        self.rank = inRank
+
+        if inRank == 11 or inRank == 12 or inRank == 13:  # Handles face cards (queen, king and jack)
+            self.value = 10
+        elif inRank == 14:  # Handles ace (this is a temporary solution as ace can be 1 and 11)
+            self.value = 11
+        else:
+            self.value = inRank
+
+        if inSuit == 0:
             self.suit = "clubs"
-        elif inputSuit == 1:
+        elif inSuit == 1:
             self.suit = "diamonds"
-        elif inputSuit == 2:
+        elif inSuit == 2:
             self.suit = "hearts"
-        elif inputSuit == 3:
+        elif inSuit == 3:
             self.suit = "spades"
-        print("Rank: "+ str(self.rank) + " Suit: " + self.suit)
+
+        #print("Rank: "+ str(self.rank) + " Value: " + str(self.value) + " Suit: " + self.suit)
 
     def getValue(self, card):
-        return self.rank
+        return self.value
 
 Deck()
