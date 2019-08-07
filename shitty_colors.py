6bd1bdd7d464cbbdc9c299068adce65bd3712eab
naff1x <naffixemail@gmail.com>
2019-08-07 12:34:59 +0200

    Created separate class for storing universally used colors.

---
+++ shitty_colors.py
@@ -0,0 +1,10 @@
+class Colors:
+    green = (0, 200, 0)
+    bright_green = (70, 255, 70)
+
+    white = (255, 255, 255)
+    dark_white = (200, 200, 200)
+
+    red = (255, 0, 0)
+    dark_red = (255, 70, 70)
+
