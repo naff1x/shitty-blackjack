c725c313c45fb5730293fdd3e68b01393a0f8f7b
naff1x <naffixemail@gmail.com>
2019-08-07 11:53:28 +0200

    Moved over basic elements for game from "crashsite.py"

---
+++ game.py
@@ -0,0 +1,38 @@
+import pygame
+import pygame_textinput
+
+
+class Game:
+    def __init__(self, window, music_status):
+        game_background = pygame.image.load("images/blackjack-table.jpg").convert()
+        bet_input = pygame_textinput.TextInput("Enter bet here")
+        window.blit(game_background, (0, 0))
+        pygame.display.flip()
+
+        clock = pygame.time.Clock()
+        game_active = True
+        music_playing = music_status
+
+        while game_active:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    game_active = False
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_p:
+                        if music_playing:
+                            pygame.mixer_music.pause()
+                            music_playing = False
+                        else:
+                            pygame.mixer_music.unpause()
+                            music_playing = True
+
+                    # Update 'gameBackground' on the main screen
+                    window.blit(game_background, (0, 0))
+
+                    if bet_input.update(events):
+                        print(bet_input.get_text())
+
+                    window.blit(bet_input.get_surface(), (650, 775))
+                    pygame.display.update()
+                    clock.tick(60)  # Sets the FPS
