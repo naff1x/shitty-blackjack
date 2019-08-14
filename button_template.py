44295594301f17f0ec9a4355928adb98f772a042
naff1x <naffixemail@gmail.com>
2019-08-14 09:17:25 +0200

    Added method for checking whether button is hovered over or not.

--- button_template.py
+++ button_template.py
@@ -3,6 +3,13 @@ import pygame
 
 class Button:
     def __init__(self, surface, f, msg, x, y, w, h, inClr, acClr):
+        self.inactive_color = inClr
+        self.active_color = acClr
+        self.xPos = x
+        self.yPos = y
+        self.width = w
+        self.height = h
+        self.surface = surface
         """
         'win' --> window object
         'surface' --> surface within window to draw on
@@ -12,12 +19,6 @@ class Button:
         'w' and 'h' --> button dimensions
         'acClr' and 'inClr' --> button's color when hovered over and not
         """
-        mouse_pos = pygame.mouse.get_pos()
-        if x+w > mouse_pos[0] > x and y+h > mouse_pos[1] > y:  # Button engaged...
-            pygame.draw.rect(surface, acClr, (x, y, w, h))
-        else:
-            pygame.draw.rect(surface, inClr, (x, y, w, h))
-
         self.button = pygame.Rect(x, y, w, h)
         self.buttonText = f.render(str(msg), 1, (0, 0, 0))  # Create text
         self.buttonTextRect = self.buttonText.get_rect()  # Create figure for text
@@ -25,3 +26,13 @@ class Button:
         self.button.centery)  # Set coordinates for the figure
 
         surface.blit(self.buttonText, self.buttonTextRect)
+
+    def engage_button(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if self.xPos + self.width > mouse_pos[0] > self.xPos and self.yPos + self.height > mouse_pos[1] > self.yPos:
+            # Button engaged...
+            pygame.draw.rect(self.surface, self.active_color, (self.xPos, self.yPos, self.width, self.height))
+            self.surface.blit(self.buttonText, self.buttonTextRect)
+        else:
+            pygame.draw.rect(self.surface, self.inactive_color, (self.xPos, self.yPos, self.width, self.height))
+            self.surface.blit(self.buttonText, self.buttonTextRect)
