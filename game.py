b38413bd0846e627cd013f8a7c3f72f0f7b9f3cd
naff1x <naffixemail@gmail.com>
2019-08-24 17:03:05 +0200

    Added rendering of cards to the table.

--- game.py
+++ game.py
@@ -1,18 +1,63 @@
 import pygame
-import cards
+from cards import *
+from players import *
 # import pygame_textinput
+import button_template
+from shitty_colors import *
 
 
 class Game:
+
+    def blit_card(self, card, isPlayer):
+        if isPlayer:  # If the given card is the player's card...
+            self.game_background.blit(card.getVisual(), (self.user_cards_xpos, 450))
+            self.user_cards_xpos += 40
+        else:  # Given card belongs to the house...
+            self.game_background.blit(card.getVisual(), (self.house_cards_xpos, 100))
+            self.house_cards_xpos += 40
+
+    def initial_draw(self):
+        for i in range(2):
+            card = self.deck.getTopCard()
+            self.user.hit(card)
+            self.blit_card(card, True)
+            card = self.deck.getTopCard()
+            self.house.hit(card)
+            self.blit_card(card, False)
+
     def __init__(self, window, music_status):
-        game_background = pygame.image.load("images/blackjack-table.jpg").convert()
+        self.game_background = pygame.image.load("images/blackjack-table.jpg").convert()
+        self.card_surface = pygame.Surface((1280, 800))
+        self.user_cards_xpos = 500
+        self.house_cards_xpos = 500
+        self.card_surface.set_alpha(0)
+
         # bet_input = pygame_textinput.TextInput("Enter bet here") # This line adds ~4s to the loading time
 
-        game_deck = cards.Deck()
-        game_deck.shuffleDeck()
         # test_card = game_deck.newDeck[0].get_visual()
-        test_card = pygame.transform.scale(game_deck.deck[0].get_visual(), (130, 180))
-        game_background.blit(test_card, (545, 450))
+        # test_card = pygame.transform.scale(game_deck.deck[0].get_visual(), (130, 180))
+        # game_background.blit(test_card, (545, 450))
+
+        self.user = Player()
+        self.house = Bank()
+
+        self.deck = Deck()
+        self.deck.shuffleDeck()
+
+        self.initial_draw()
+
+        # Add font
+        comic_sans = pygame.font.Font("fonts/Comic Sans MS.ttf", 40)
+
+        # Add buttons
+        self.buttons = []
+        self.hit_button = button_template.Button(self.game_background, comic_sans, "hit", 200, 355, 150, 90,
+                                            Colors.bright_green, Colors.green)
+
+        self.buttons.append(self.hit_button)
+
+
+        # game_background.blit(self.user.hand.cards[0].getVisual(), (545, 450))
 
         clock = pygame.time.Clock()
         game_active = True
@@ -31,9 +76,17 @@ class Game:
                         else:
                             pygame.mixer_music.unpause()
                             music_playing = True
+                if pygame.mouse.get_pressed()[0] and self.hit_button.button.collidepoint(pygame.mouse.get_pos()):
+                    print("HIT")
+                    card = self.deck.getTopCard()
+                    self.user.hit(card)
+                    self.blit_card(card, True)
+                if event.type == pygame.MOUSEMOTION:  # When the mouse moves, check for engagement in each button
+                    for i in range(len(self.buttons)):
+                        self.buttons[i].engage_button()
 
             # Update 'gameBackground' on the main screen
-            window.blit(game_background, (0, 0))
+            window.blit(self.game_background, (0, 0))
 
             """if bet_input.update(events):
                 print(bet_input.get_text())
