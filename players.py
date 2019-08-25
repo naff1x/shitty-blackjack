b254d39e159e6f3bbf48e58150b42c0826708ead
naff1x <naffixemail@gmail.com>
2019-08-25 16:42:45 +0200

    Added handling of aces.

--- players.py
+++ players.py
@@ -5,6 +5,12 @@ class Bank:
         self.hand = Hand()
 
     def hit(self, card):
+        if card.getValue() is 14:  # If the card is an ace...
+            if self.getValue() + 11 > 21:  # If the ace's 11 would result in a loss...
+                card.value = 1
+            else:
+                card.value = 11
+
         self.hand.addCard(card)
 
     def getValue(self):
@@ -13,10 +19,16 @@ class Bank:
 class Player:
     def __init__(self):
         self.hand = Hand()
-        self.money = 1000;
+        self.money = 1000
         # insats
 
     def hit(self, card):
+        if card.getValue() is 14:  # If the card is an ace...
+            if self.getValue()+11 > 21:  # If the ace's 11 would result in a loss...
+                card.value = 1
+            else:
+                card.value = 11
+
         self.hand.addCard(card)
 
     def getValue(self):
